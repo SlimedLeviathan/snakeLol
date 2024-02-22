566188095f43d3ce4f660214e82e3657f6ab7e48
SlimedLeviathan <cantonsteven6@gmail.com>
2024-02-22 13:07:15 -0500

    done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # the main folder, run the game from here
 
 import pygame as pg
-from tile import Tile
+import tile as t
 
 pg.init()
 
@@ -12,16 +12,49 @@ screen = pg.display.set_mode((xTiles * 50, yTiles * 50))
 
 running = True
 started = False # this makes sure the game doesnt start until the player moves
+killed = False
 
 tileArray = [[] for _ in range(xTiles)] # accessing any tile is [x][y]
 
-for xNum in range(xTiles):
-    for yNum in range(yTiles):
-        tileArray[xNum].append(Tile(xNum, yNum))
+pg.time.set_timer(pg.USEREVENT + 1, 1) # every milisecond we can get this event
+
+snakeFrames = 500 # this is how many frames it takes for the snake to move, and it can go down the longer the game lasts
+snakeFrame = 0
+
+applesEaten = 0
+
+def startGame(): # does everything needed for the game to start
+    global snakeFrame, snakeFrames, started, tileArray, applesEaten
+
+    snakeFrames = 500 # this is how many frames it takes for the snake to move, and it can go down the longer the game lasts
+    snakeFrame = 0
+
+    applesEaten = 0
+
+    started = False # this makes sure the game doesnt start until the player moves
+    
+    # gets rid of the previous apples and heads
+    t.apples.clear()
+    t.heads.clear()
+
+    tileArray = [[] for _ in range(xTiles)] # accessing any tile is [x][y]
+
+    for xNum in range(xTiles):
+        for yNum in range(yTiles):
+            tileArray[xNum].append(t.Tile(xNum, yNum))
+
+    t.Apple(4, 2, tileArray)
+
+    mainhead = t.SnakeHead(4, 4, tileArray)
+
+    # adds 2 body parts automatically
+    mainhead.addBody(tileArray)
+    mainhead.addBody(tileArray)
+
+startGame() # does the first start of the game
 
 while running == True:
-
-    for xTiles in tileArray:
+    for xTiles in tileArray: # place all tiles onto the screen
         for tile in xTiles:
             if (tile.obj == None): # if there is nothing on the tile, we cont use the tiles obj as its color, so we make it black
                 pg.draw.rect(screen, [0,0,0], [tile.x * 50, tile.y * 50, 50, 50]) # draws the main color of the tile
@@ -31,13 +64,57 @@ while running == True:
                 pg.draw.rect(screen, tile.obj.color, [tile.x * 50, tile.y * 50, 50, 50]) # draws the main color of the tile
                 pg.draw.rect(screen, [255,255,255], [tile.x * 50, tile.y * 50, 50, 50], 1) # draws the small outline
 
+    for apple in t.apples:
+        if (apple.eaten == True):
+            apple.place(tileArray) # places the apple randomly if it was eaten
+
+            applesEaten += 1
+
     for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            moved = False
+
+            for head in t.heads:
+                if (head.direction != "dead"): # keeps the head dead
+                    if event.key == pg.K_w: # up
+                        head.changeDirection(tileArray, "up")
+                        moved = True
+
+                    elif event.key == pg.K_s: # down
+                        head.changeDirection(tileArray, "down")
+                        moved = True
+
+                    elif event.key == pg.K_a: # left
+                        head.changeDirection(tileArray, "left")
+                        moved = True
+
+                    elif event.key == pg.K_d: # right
+                        head.changeDirection(tileArray, "right")
+                        moved = True
+
+            # to make sure the game starts
+            if (moved == True and started == False):
+                started = True
+
         if event.type == pg.KEYUP:
             if event.key == pg.K_ESCAPE:
                 running = False
 
+            if event.key == pg.K_r:
+                startGame() # resets the game if the r button is pressed
+
         if event.type == pg.QUIT:
             running = False
+
+        if event.type == pg.USEREVENT + 1:
+            if killed == False and started == True:
+                snakeFrame += 1
+
+                if (snakeFrame >= snakeFrames - (applesEaten * 3)):
+                    snakeFrame = 0
+
+                    for head in t.heads:
+                        head.move(tileArray)
 
     pg.display.flip() # displays all changes tot he screen
 
--- tile.py
+++ tile.py
@@ -1,4 +1,8 @@
 # this file contains the tile class and all objects, which is how everything works
+import random
+
+rand = random.Random()
+
 def getDirection(direction): # really easy function
     if direction == "up":
         return [0,-1]
@@ -9,6 +13,8 @@ def getDirection(direction): # really easy function
     elif direction == "right":
         return [1,0]
 
+    return [0,0] # for anything else
+
 class Tile:
     def __init__(self, x, y):
         self.x = x
@@ -17,47 +23,124 @@ class Tile:
         self.obj = None # whats ontop of the tile
 
 class TileObject:
-    def __init__(self, x, y, name, color, tileArray):
+    def __init__(self, x, y, color, tileArray):
         self.x = x
         self.y = y
         
         self.color = color
 
-        self.name = name
-
         tileArray[x][y].obj = self # sets the tiles obj to this obj
-
-# this is where the player controls the snake from
-class SnakeHead(TileObject): # only one of these SHOULD be made, but ill make a mode for multiple
-    def __init__(self, x, y, child, tileArray):
-        TileObject.__init__(self, x, y, "head", [50,255,50], tileArray)
-
-        self.direction = "up" # the direction of the snakes movement, dictated by the player
-
-        self.child = child # the next body part in line
-
-    def addBody(self, tileArray): # makes a new body
-        if (self.child == None): # if the head doesnt have one it adds it to behind where its going
-            forces = getDirection(self.direction)
-
-            self.child = SnakeBody(self.x + -forces[0], self.y + -forces[1], self.direction, tileArray) # makes a new snake body
-
-        else: # otherwise it tells the next bodies to do the same
-            self.child.addBody()
 
 class SnakeBody(TileObject): # this is the other tiles of the player
     def __init__(self, x, y, tileArray, direction, child = None):
-        TileObject.__init__(self, x, y, "body", [0,255,0], tileArray)
+        TileObject.__init__(self, x, y, [0,255,0], tileArray)
 
         self.direction = direction # the direction the snakes body will go next
 
         self.child = child
 
-    def addBody(self, tileArray): # same as snake head add body
-        if (self.child == None): # if no child, add behind it
+    def addBody(self, tileArray): # makes a new body
+        if (self.child == None): # if the head doesnt have one it adds it to behind where its going
             forces = getDirection(self.direction)
 
-            self.child = SnakeBody(self.x + -forces[0], self.y + -forces[1], self.direction, tileArray) # makes a new snake body
+            self.child = SnakeBody(self.x + -forces[0], self.y + -forces[1], tileArray, self.direction) # makes a new snake body
 
-        else: # otherwise it tells the next body to do it
-            self.child.addBody()
+        else: # otherwise it tells the next bodies to do the same
+            if (self.child.direction == "dead"):
+                self.child = None # basically tell the program to redo this without looking at the child
+                self.addBody(tileArray)
+
+            else:
+                self.child.addBody(tileArray)
+
+    def move(self, tileArray):
+        force = getDirection(self.direction)
+
+        force[0] += self.x
+        force[1] += self.y
+
+        if not (self.x >= len(tileArray) - 1 or self.y >= len(tileArray[0]) - 1 or self.x <= 0 or self.y <= 0): # making sure the snake doesnt go outside the bounds of the game
+            tile = tileArray[force[0]][force[1]]
+
+            if (tile.obj != None): # if there is something on the tile, we do the hit function
+                tile.obj.hit(self, tileArray)
+
+            else:
+                # the tile this body was just on should have nothing on it 
+                tileArray[self.x][self.y].obj = None
+
+                tile.obj = self # and otherwise this snake body inhabits it
+                
+                self.x = force[0]
+                self.y = force[1]
+
+                if (self.child != None):
+                    self.child.move(tileArray)
+
+                    if (self.child.direction != "dead"): # keeps them dead
+                        self.child.direction = self.direction # after the child moves in the direction it is supposed to, we set its direction to this ones now, which allows the snake to turn
+
+                    else:
+                        self.child = None
+
+        else:
+            self.kill()
+
+    def hit(self, snake, tileArray):
+        snake.kill()
+
+    def kill(self):
+        if (self.child != None):
+            self.child.kill()
+
+        self.color = [150, 150, 150] # gray to represent death
+
+        self.direction = "dead" # should stop the snake from moving
+
+heads = []
+# this is where the player controls the snake from
+class SnakeHead(SnakeBody): # only one of these SHOULD be made, but ill make a mode for multiple
+    def __init__(self, x, y, tileArray, direction = "up"):
+        SnakeBody.__init__(self, x, y, tileArray, direction)
+
+        heads.append(self)
+
+    def changeDirection(self, tileArray, direction):
+        force = getDirection(direction)
+
+        tile = tileArray[self.x + force[0]][self.y + force[1]]
+
+        if (tile.obj != self.child or tile.obj == None):
+            self.direction = direction
+
+apples = []
+class Apple(TileObject):
+    def __init__(self, x, y, tileArray):
+        TileObject.__init__(self, x, y, [255,0,0], tileArray)
+
+        self.eaten = False # so that the apple can gets set after the snake is made
+
+        apples.append(self)
+
+    def hit(self, snake, tileArray):
+        self.eaten = True # set up for the apple to move to a different spot
+        tileArray[self.x][self.y].obj = None # make sure the apple get destroyed
+
+        snake.addBody(tileArray)
+        snake.move(tileArray) # this allows the snake to continue moving
+
+    def place(self, tileArray):
+        xRange = len(tileArray)
+        yRange = len(tileArray[0])
+        
+        while (self.eaten == True):
+            randX = rand.randint(0, xRange - 1) # gets random placement
+            randY = rand.randint(0, yRange - 1)
+
+            if (tileArray[randX][randY].obj == None): # checks that its empty
+                tileArray[randX][randY].obj = self # places itself
+
+                self.x = randX
+                self.y = randY
+
+                self.eaten = False # and stops the loop
